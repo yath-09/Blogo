6b5524129121e3967e312917e4da108d61ffdf07
Yatharth <yatharth0903@gmail.com>
2024-02-03 00:13:28 +0530

    add:auth and signup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import  express  from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
+
 dotenv.config()
 const app=express()
 
-
+app.use(express.json())
 /* MONGOOSE SETUP */
 const PORT = 3000;
 mongoose
@@ -16,3 +19,18 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
+
+
+  app.use('/api/user',userRoutes)
+  app.use('/api/auth',authRoutes)
+
+  app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal server error"
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+
+  })
