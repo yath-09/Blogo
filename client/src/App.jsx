914996529fc33a7343b2cf19d7a52e9c18306bfb
Yatharth <yatharth0903@gmail.com>
2024-02-02 20:14:54 +0530

    add:header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import Project from "./pages/Project"
+import Header from "./Components/Header"
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-red-300'>hello</h1>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Project/>}/>
+      </Routes>
+
+    </BrowserRouter>
+
     </>
   )
 }
