6c44d0953e8d48730f484d1929ed04c62f3fae06
Yatharth <yatharth0903@gmail.com>
2024-02-07 20:46:24 +0530

    added: like comment

--- client/src/Components/CommentSection.jsx
+++ client/src/Components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
 import { useState,useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate} from 'react-router-dom';
 import Comment from './Comment';
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
@@ -52,6 +52,34 @@ export default function CommentSection({ postId }) {
     getComments();
   }, [postId]);
 
+  const navigate=useNavigate()
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -115,7 +143,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
