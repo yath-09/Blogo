65d3c6a4e492d42df729d06076dd721d4fea3974
Yatharth <yatharth0903@gmail.com>
2024-02-13 01:11:16 +0530

    bug:file name

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import {useLocation} from 'react-router-dom'
 import DashSidebar from '../Components/DashSidebar';
 import DashProfile from '../Components/DashProfile';
 import DashPosts from '../Components/DashPosts';
-import DashUsers from '../Components/dashUsers';
+import DashUsers from '../Components/DashUsers';
 import DashComments from '../Components/DashComments';
 import DashboardComponent from '../Components/DashboardComponent';
 
